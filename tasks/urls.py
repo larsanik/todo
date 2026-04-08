733ea7fc893ecabcf9aa08aa099a26e8c4fc9502
larsanik <larsanik@gmail.com>
2026-04-08 11:18:13 +0300

    feat: added todo items list view

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 
-from tasks.views import TodoListListView
+from tasks.views import TodoListListView, TodoItemListView
 
 urlpatterns = [
-    path('', TodoListListView.as_view(), name='list'),
+    path('', TodoListListView.as_view(), name='index'),
+    path('list/<int:list_id>', TodoItemListView.as_view(), name='list'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -2,7 +2,8 @@ from django.shortcuts import render
 from django.urls import reverse_lazy
 from django.views.generic import ListView
 from django.contrib.auth.mixins import LoginRequiredMixin
-from tasks.models import TodoList
+from tasks.models import TodoList, TodoItem
+from django.core.exceptions import PermissionDenied
 
 
 # Create your views here.
@@ -12,3 +13,13 @@ class TodoListListView(LoginRequiredMixin, ListView):
 
     def get_queryset(self):
         return TodoList.objects.for_user(self.request.user)
+
+class TodoItemListView(LoginRequiredMixin, ListView):
+    login_url = reverse_lazy('account_login')
+    template_name = 'tasks/todo_list.html'
+
+    def get_queryset(self):
+        todo_list = TodoList.objects.for_user(self.request.user).filter(pk=self.kwargs['list_id'])
+        if todo_list is None:
+            raise PermissionDenied()
+        return TodoItem.objects.filter(todo_list_id=self.kwargs['list_id'])
