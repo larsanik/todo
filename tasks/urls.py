0d1797b26bbf9910fb5732a15ffd81b67d9f972c
larsanik <larsanik@gmail.com>
2026-04-08 16:58:15 +0300

    feat: редактор задач не работает(((((

--- tasks/urls.py
+++ tasks/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
 
-from tasks.views import TodoListListView, TodoItemListView, TodoListCreateView
+from tasks.views import TodoListListView, TodoItemListView, TodoListCreateView, TodoItemCreateView
 
 urlpatterns = [
     path('', TodoListListView.as_view(), name='index'),
     path('list/<int:list_id>', TodoItemListView.as_view(), name='list'),
     path('list/add/', TodoListCreateView.as_view(), name='list-add'),
+    path('list/<int:list_id>/item/add/', TodoItemCreateView.as_view(), name='item-add'),
 ]
--- tasks/views.py
+++ tasks/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
 from django.views.generic import ListView, CreateView
 from django.contrib.auth.mixins import LoginRequiredMixin
 from tasks.models import TodoList, TodoItem
@@ -24,6 +24,13 @@ class TodoItemListView(LoginRequiredMixin, ListView):
             raise PermissionDenied()
         return TodoItem.objects.filter(todo_list_id=self.kwargs['list_id'])
 
+    def get_context_data(self, *,object_list=None, **kwargs):
+        context = super().get_context_data(object_list=object_list, **kwargs)
+        todo_list=TodoList.objects.get(pk=self.kwargs['list_id'])
+        context['todo_list'] = todo_list
+        return context
+
+
 class TodoListCreateView(LoginRequiredMixin, CreateView):
     model = TodoList
     fields = ['title']
@@ -31,3 +38,16 @@ class TodoListCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.owner = self.request.user
         return super().form_valid(form)
+
+class TodoItemCreateView(LoginRequiredMixin, CreateView):
+    model = TodoItem
+    fields = ['todo_list', 'title', 'description', 'due_date']
+
+    def get_initial(self):
+        initial_data = super().get_initial()
+        todo_list = TodoList.objects.for_user(user=self.request.user).get(id=self.kwargs['list_id'])
+        initial_data['todo_list'] = todo_list
+        return initial_data
+
+    def get_success_url(self):
+        return reverse('list', args=[self.object.todo_list_id])
